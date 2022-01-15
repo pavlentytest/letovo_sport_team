011ef4c62a34aeab14d8b2457ff458459de9f7ce
Pavel <impetus@yandex.ru>
2022-01-15 12:12:19 +0300

    Create well designed project!

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+
+
+public class Car {
+    public int a; // 0
+    public String str; // ""
+    public double b; //0.0
+    public boolean flag; // false
+
+    public Car() {
+        this.a = 100;
+
+    }
+}
---
+++ src/Letovo.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class Letovo {
+
+     public static void main(String[] args) {
+        int[] a = new int[6]; // 0 0 0 0 0 0
+        double x = Math.random();
+        System.out.println(x);
+        Random r = new Random();
+        for(int i=0;i<a.length;i++) {
+            a[i] = r.nextInt(100);
+        }
+        printArray(a);
+
+      /*  Car bmw = new Car();
+        int aa = bmw.a; // 0
+         // System.out.println(bmw.flag);
+         Intellij IDEA shortcuts
+       */
+    }
+    public static void printArray(int[] x) {
+        for(int i=0;i<x.length;i++) {
+            System.out.print(x[i] + " ");
+        }
+    }
+
+}
